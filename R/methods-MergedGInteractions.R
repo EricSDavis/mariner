ef5acce3b95fe61ed1648e1c3379a02ef646a5a0
Eric Davis <ericscottdavis@outlook.com>
2022-06-22 09:00:46 -0400

    Add accessor and tests for allPairs.

--- R/methods-MergedGInteractions.R
+++ R/methods-MergedGInteractions.R
@@ -1,5 +1,43 @@
 ## Accessors -------------------------------------------------------------------
 
+#' Internal accessor function for allPairs
+#' @inheritParams allPairs
+#' @importFrom S4Vectors isEmpty
+#' @importFrom glue glue
+#' @importFrom rlang abort
+.allPairs <- function(x, source) {
+
+    ## Pull out all pairs
+    res <- x@allPairs
+
+    if (!missing(source)) {
+
+        ## Filter by source
+        res <- res[src == source]
+
+        ## Catch empty error
+        if (isEmpty(res)) {
+            msg <- c(glue("Source '{source}' not found."),
+                     'i' = glue("Did you mean one of these: ",
+                                "{paste0(x = unique(x@allPairs$src),
+                                         collapse=', ')}?"))
+            abort(msg)
+        }
+    }
+
+    ## Remove other columns
+    res <- res[,-c("src", "id", "grp", "clst")]
+
+    return(as_ginteractions(res))
+}
+
+#' Get all pairs from MergedGInteractions object
+#' @inheritParams selectionMethod
+#' @export
+setMethod("allPairs", signature(x = "MergedGInteractions",
+                                source = "character_OR_numeric_OR_missing"),
+          definition = .allPairs)
+
 #' Get selectionMethod from MergedGInteractions object
 #'
 #' @param x MergedGInteractions object.
