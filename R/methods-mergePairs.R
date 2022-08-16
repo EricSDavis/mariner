bc3cb128774363eb09b7c58aad43c24affc7f455
Eric Davis <ericscottdavis@outlook.com>
2022-08-15 23:10:25 -0400

    Begin refactor mergePairs.
    Only accepts lists of GInteractions objects.
    Upstream binning no longer required.
    Mean of modes uses starts and ends.
    mergePairs takes method and radius.

--- R/methods-mergePairs.R
+++ R/methods-mergePairs.R
@@ -11,18 +11,15 @@
         abort("List must have length > 0.")
     }
 
-    ## Test for each type of list input
-    if (is(x[[1]], 'data.frame')) {
-        listType <- 'data.frame'
-    } else if (is(x[[1]], 'DFrame')) {
-        listType <- class(x[[1]])
-    } else if (is(x[[1]], 'GInteractions')) {
+    ## Ensure every element of list
+    ## is GInteractions
+    if (is(x[[1]], 'GInteractions')) {
         listType <- class(x[[1]])
     } else {
         listType <- unique(unlist(lapply(x, \(u) class(u))))
         msg <- c("Incorrect list input type.",
-                 'i' = glue("Input must be data.frame-like ",
-                            "object or a GInteractions object."),
+                 'i' = glue("Input must be a list ",
+                            "of GInteractions objects."),
                  'x' = glue("Your list is type ",
                             "{paste(listType, collapse=', ')}."))
         abort(msg)
@@ -43,6 +40,57 @@
                             "{paste(types, collapse=', ')}"))
         abort(msg)
     }
+
+}
+
+#' Internal function for reading in lists of GInteractions
+#' or GInteractions objects.
+#' @inheritParams mergePairs
+#' @importFrom data.table data.table as.data.table
+#' @returns A concatenated data.table from the list
+#' @noRd
+.readGInteractionsList <- function(x, column) {
+    ## Handle list or single GInteractions object
+    if (is(x, "list")) {
+
+        ## Check that list is formatted correctly
+        .checkListFormat(x)
+
+        ## Convert to list of data.tables
+        bedpe <- lapply(x, as.data.table)
+
+    } else {
+
+        ## Convert to list of data.tables
+        bedpe <- list(as.data.table(x))
+
+    }
+
+    ## Rename columns to prevent overwriting
+    newColumnNames <-
+        lapply(bedpe, colnames) |>
+        lapply(\(x) gsub("(^src$|^id$|^grp$|^clst$)", "\\1_1", x))
+    bedpe <- Map(setNames, bedpe, newColumnNames)
+
+    ## Add new column for source
+    if (!is.null(names(bedpe))) {
+        bedpe <- Map(cbind, bedpe, src = names(bedpe))
+    } else {
+        bedpe <- Map(cbind, bedpe, src = seq_along(bedpe))
+    }
+
+    ## Concatenate bedpe
+    dt <- do.call(rbind, bedpe)
+
+    ## Check column name exists in bedpe
+    if (!missing(column)) {
+        if (!column %in% colnames(dt)) {
+            abort(glue("Column '{column}' does not exist."))
+        }
+    }
+
+    ## Return concatenated data.table
+    return(dt)
 }
 
 #' Read in Bedpe from list of objects
@@ -54,45 +102,47 @@
 #' @importFrom data.table as.data.table
 #' @return A list of data.tables
 #' @noRd
-.readBedpeFromList <- function(x) {
-
-    ## Transform multiple times to make
-    ## the structure consistent.
-    lapply(x, \(u){
-        u |>
-            as.data.table() |>
-            makeGInteractionsFromDataFrame() |>
-            as.data.table()
-    })
-
-}
+# .readBedpeFromList <- function(x) {
+#
+#     ## Transform multiple times to make
+#     ## the structure consistent.
+#     lapply(x, \(u){
+#         u |>
+#             as.data.table() |>
+#             makeGInteractionsFromDataFrame() |>
+#             as.data.table()
+#     })
+#
+# }
 
 #' Find clusters by manhattan distance with DBSCAN
 #' @param x `data.table` with at least 'start1' and
-#'  'start2' columns. Data should be binned to
-#'  the `binSize` parameter.
-#' @param radius manhattan distance used to define
-#'  a cluster of pairs.
-#' @param binSize Integer (numeric) describing the
-#'  resolution (range widths) of the paired data.
+#'  'start2' columns.
+#' @inheritParams mergePairs
 #'
 #' @importFrom dbscan dbscan
 #' @noRd
-.findClusters <- function(x, radius, binSize) {
-    d <- dist(x/binSize, method = 'manhattan')
+.findClusters <- function(x, radius, method) {
+    d <- dist(x, method = method)
     dbscan(d, eps = radius, minPts = 2)$cluster
 }
 
 #' Calculate new paired ranges with the mean of modes method
 #' @param start1 vector of start1 positions
 #' @param start2 vector of start2 positions
-#' @param binSize Integer (numeric) describing the
-#'  resolution (range widths) of the paired data.
+#' @param end1 vector of end1 positions
+#' @param end2 vector of end2 positions
+#'
+#' Calculates the mean of modes. Results are rounded
+#' to the nearest whole number.
+#'
+#' @returns list of updated start/end positions
 #' @noRd
-.newPairRanges <- function(start1, start2, binSize) {
-    s1 <- .meanOfModes(start1, binSize)
-    s2 <- .meanOfModes(start2, binSize)
-    list(start1=s1, end1=s1+binSize, start2=s2, end2=s2+binSize)
+.newPairRanges <- function(start1, start2, end1, end2) {
+    list(start1=round(mean(.modes(start1))),
+         end1=round(mean(.modes(end1))),
+         start2=round(mean(.modes(start2))),
+         end2=round(mean(.modes(end2))))
 }
 
 #' Internal function for renaming mergePairs columns
@@ -106,40 +156,18 @@
         gsub("^clst_1$", "clst", x = _)
 }
 
+
+
 #' Internal mergePairs function
 #' @inheritParams mergePairs
 #' @importFrom rlang inform
 #' @importFrom data.table as.data.table uniqueN `:=`
 #' @noRd
-.mergePairs <- function(x, binSize, radius, column) {
+.mergePairs <- function(x, radius, method, column, selectMax) {
 
-    ## Concatenate bedpe
-    bedpe <- do.call(rbind, x)
-
-    ## Check column name exists in bedpe
-    if (!missing(column)) {
-        if (!column %in% colnames(bedpe)) {
-            abort(glue("Column '{column}' does not exist."))
-        }
-    }
-
-    ## Convert to a GInteractions object for binning
-    bedpe <- makeGInteractionsFromDataFrame(bedpe)
-
-    ## Ensure interactions are binned
-    binned <- .checkBinnedPairs(bedpe, binSize = binSize)
-    if (!binned) {
-        bedpe <- binPairs(x = bedpe,
-                          binSize = binSize)
-        msg <- c("Pairs are not binned to `binSize`.",
-                 'i' = glue("Binning with binSize={binSize}, ",
-                            "pos1='center' and pos2='center'."),
-                 'i' = glue("Use `binPairs()` for different binning."))
-        inform(msg)
-    }
-
-    ## Convert to data.table format
-    dt <- as.data.table(bedpe)
+    ## Parse list or GInteractions input and return
+    ## as a concatenated data.table
+    dt <- .readGInteractionsList(x, column)
 
     ## Annotate id & groups (essentially pairs of chromosomes)
     dt[,id := .I]
@@ -158,7 +186,7 @@
         pb$tick(tokens=list(step="Merging pairs"));
         .findClusters(x = .SD[,c('start1','start2')],
                       radius = radius,
-                      binSize = binSize)},
+                      method = method)},
        by = grp]
 
     ## Separate unique pairs by denoting as negative integers
@@ -176,16 +204,14 @@
            (columnsToUpdate) :=
                .newPairRanges(start1 = .SD[['start1']],
                               start2 = .SD[['start2']],
-                              binSize = binSize),
+                              end1 = .SD[['end1']],
+                              end2 = .SD[['end2']]),
            by = .(grp, clst)]
 
         ## Select the first of the duplicated pairs
         single <- dt[clst < 0]
         selected <- dt[dt[clst > 0,.I[1], by = .(grp, clst)]$V1]
         mergedPairs <- rbind(single, selected)
-
-        ## Remove metadata (since the choice is arbitrary)
-        mergedPairs[,colnames(mcols(bedpe)) := NULL]
 
     } else {
         selectionMethod <- glue("Select by column '{column}'")
@@ -213,6 +239,10 @@
     ## Return to original column names
     colnames(mcols(mergedPairs)) <- .renameCols(colnames(mcols(mergedPairs)))
 
+    ## Remove metadata if using mean of modes
+    ## since the choice is arbitrary.
+    if (missing(column)) mcols(mergedPairs) <- NULL
+
     ## Build MergedGInteractions object
     obj <-
         MergedGInteractions(delegate = mergedPairs,
@@ -227,86 +257,94 @@
 #' Internal mergePairs for list type
 #' @inheritParams mergePairs
 #' @noRd
-.mergePairsList <- function(x, binSize, radius, column) {
-
-    ## Check that list is formatted correctly
-    .checkListFormat(x)
-
-    ## Read in Bedpe
-    bedpe <- .readBedpeFromList(x)
-
-    ## Rename columns to prevent overwriting
-    newColumnNames <-
-        lapply(bedpe, colnames) |>
-        lapply(\(x) gsub("(^src$|^id$|^grp$|^clst$)", "\\1_1", x))
-    bedpe <- Map(setNames, bedpe, newColumnNames)
-
-    ## Add new column for source
-    if (!is.null(names(bedpe))) {
-        bedpe <- Map(cbind, bedpe, src = names(bedpe))
-    } else {
-        bedpe <- Map(cbind, bedpe, src = seq_along(bedpe))
-    }
-
-    ## Pass to internal merging function
-    .mergePairs(x = bedpe,
-                binSize = binSize,
-                radius = radius,
-                column = column)
-}
+# .mergePairsList <- function(x, binSize, radius, column) {
+#
+#     ## Check that list is formatted correctly
+#     .checkListFormat(x)
+#
+#     ## Read in Bedpe
+#     bedpe <- .readBedpeFromList(x)
+#
+#     ## Rename columns to prevent overwriting
+#     newColumnNames <-
+#         lapply(bedpe, colnames) |>
+#         lapply(\(x) gsub("(^src$|^id$|^grp$|^clst$)", "\\1_1", x))
+#     bedpe <- Map(setNames, bedpe, newColumnNames)
+#
+#     ## Add new column for source
+#     if (!is.null(names(bedpe))) {
+#         bedpe <- Map(cbind, bedpe, src = names(bedpe))
+#     } else {
+#         bedpe <- Map(cbind, bedpe, src = seq_along(bedpe))
+#     }
+#
+#     ## Pass to internal merging function
+#     .mergePairs(x = bedpe,
+#                 binSize = binSize,
+#                 radius = radius,
+#                 column = column)
+# }
 
 #' Internal mergePairs for character type
 #' @inheritParams mergePairs
 #' @importFrom data.table fread
 #' @noRd
-.mergePairsCharacter <- function(x, binSize, radius, column) {
-
-    ## Read in files as list of bedpe
-    bedpe <- lapply(x, fread)
-
-    ## Rename columns to prevent overwriting
-    newColumnNames <-
-        lapply(bedpe, colnames) |>
-        lapply(\(x) gsub("(^src$|^id$|^grp$|^clst$)", "\\1_1", x))
-    bedpe <- Map(setNames, bedpe, newColumnNames)
-
-    ## Add new column for source
-    bedpe <- Map(cbind, bedpe, src = basename(x))
-
-    ## Pass to internal merging function
-    .mergePairs(x = bedpe,
-                binSize = binSize,
-                radius = radius,
-                column = column)
-}
+# .mergePairsCharacter <- function(x, binSize, radius, column) {
+#
+#     ## Read in files as list of bedpe
+#     bedpe <- lapply(x, fread)
+#
+#     ## Rename columns to prevent overwriting
+#     newColumnNames <-
+#         lapply(bedpe, colnames) |>
+#         lapply(\(x) gsub("(^src$|^id$|^grp$|^clst$)", "\\1_1", x))
+#     bedpe <- Map(setNames, bedpe, newColumnNames)
+#
+#     ## Add new column for source
+#     bedpe <- Map(cbind, bedpe, src = basename(x))
+#
+#     ## Pass to internal merging function
+#     .mergePairs(x = bedpe,
+#                 binSize = binSize,
+#                 radius = radius,
+#                 column = column)
+# }
 
 #' Merge sets of paired interactions
 #'
 #' Sets of paired range objects (like `GInteractions`
 #' or BEDPE-formatted `data.frame`-like objects) are
-#' merged by genomic distance with dbscan.
+#' first clustered by genomic distance with `dbscan`,
+#' then a representative interaction is selected for
+#' each cluster.
 #'
 #' Interactions are clustered into groups using the
-#' `binSize`, `dist_method`, and `minPts` parameters
-#' passed to `dbscan()`. A column and function provided
-#' by the user is then used to select which interaction
-#' should be chosen to represent the group. The resulting
-#' object reports the source (which file or list
-#' name/index) of the chosen interaction.
+#' provided base pair `radius`, and distance `method`
+#' with `dbscan()`. Representative interactions are
+#' selected for each group by one of two methods.
+#' If `column` and `selectMax` arguments are provided,
+#' the representative interaction with the maximum
+#' (or minimum) value in `column` is returned for each
+#' cluster. If these parameters are missing, new ranges
+#' for each pair are returned by calculating the median
+#' of modes for each cluster.
 #'
-#' @param x Either a list of `GInteractions` objects,
-#'  a list of `data.frame`-like objects, or a list of
-#'  file paths of BEDPE-formatted data to be merged.
-#' @param binSize Integer (numeric) describing the
-#'  resolution (range widths) of the paired data.
-#' @param radius manhattan distance used to define
-#'  a cluster of pairs. Used to determine the epsilon
-#'  value for `dbscan()` (i.e. `eps = radius*binSize`).
-#' @param column Integer or character denoting the
-#'  column to be used to select among clustered
-#'  interactions.
+#' @param x List of `GInteractions` or `data.frame`-like
+#'  objects.
+#' @param radius Numeric describing the distance in base
+#'  pairs used to define a cluster or pairs.
+#' @param method Character describing the distance measure
+#'  to be used. This must be one of "euclidean", "maximum",
+#'  "manhattan", "canberra", "binary" or "minkowski".
+#'  Any unambiguous substring can be given.
+#'  Default is "manhattan".
+#' @param column Character denoting the column to be used
+#'  to select among clustered interactions.
+#' @param selectMax Logical. TRUE (default) uses `which.max()`
+#'  to select the interaction pair. FALSE uses `which.min()`.
+#'  Only applicable when `column` is specified.
 #'
-#' @return Returns a merged `GInteractions` object.
+#' @return Returns a `MergedGInteractions` object.
 #'
 #' @examples
 #' ## Reference BEDPE files (loops called with SIP)
@@ -315,25 +353,16 @@
 #'     list.files(pattern = "Loops.txt", full.names = TRUE)
 #'
 #' x <- mergePairs(x = bedpeFiles,
-#'                 binSize = 5e03,
-#'                 radius = 2,
+#'                 radius = 10e03,
 #'                 column = "APScoreAvg")
 #' x
 #'
 #' @rdname mergePairs
 #' @export
 setMethod("mergePairs",
-          signature(x = 'list',
-                    binSize = 'numeric_OR_missing',
-                    radius = 'numeric_OR_missing',
-                    column = 'character_OR_missing'),
-          definition = .mergePairsList)
-
-#' @rdname mergePairs
-#' @export
-setMethod("mergePairs",
-          signature(x = 'character',
-                    binSize = 'numeric_OR_missing',
-                    radius = 'numeric_OR_missing',
-                    column = 'character_OR_missing'),
-          definition = .mergePairsCharacter)
+          signature(x = 'list_OR_GInteractions',
+                    radius = 'numeric',
+                    method = 'character_OR_missing',
+                    column = 'character_OR_missing',
+                    selectMax = 'logical_OR_missing'),
+          definition = .mergePairs)
