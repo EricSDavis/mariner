43fd4d07a74ea38ff66b43885a3465078e07a16d
Eric Davis <ericscottdavis@outlook.com>
2022-09-23 17:28:27 -0400

    New method for snapping to bins.

--- tests/testthat/test_snapToBins.R
+++ tests/testthat/test_snapToBins.R
@@ -27,6 +27,78 @@ test_that("GRanges snap correctly to bins", {
         )
 })
 
+test_that("Upper limit snaps correctly", {
+
+    x <- GRanges("11:63395000-63400000")
+    snapToBins(x, binSize = 50e03)
+
+    x <-
+        GRanges(seqnames = "chr1",
+                ranges = IRanges(start = c(1, 9, 8),
+                                 end = c(2, 10, 11)))
+    snapToBins(x, binSize = 10)
+
+    x <- GRanges("1:9-10")
+    snapToBins(x, binSize = 10)
+    x <- GRanges("1:1-2")
+    x <- GRanges("1:11-12")
+
+
+    ## If ranges have a preferred side then use that side
+    x <- GRanges("1:1-2")
+    snapToBins(x, binSize = 10) |>
+        expect_identical(GRanges("1:0-10"))
+
+    x <- GRanges("1:9-10")
+    snapToBins(x, binSize = 10) |>
+        expect_identical(GRanges("1:0-10"))
+
+    x <- GRanges("1:8-11")
+    snapToBins(x, binSize = 10) |>
+        expect_identical(GRanges("1:0-10"))
+
+    x <- GRanges("1:9-12")
+    snapToBins(x, binSize = 10) |>
+        expect_identical(GRanges("1:10-20"))
+
+    x <- GRanges("1:5-11")
+    snapToBins(x, binSize = 10) |>
+        expect_identical(GRanges("1:0-10"))
+
+    x <- GRanges("1:9-15")
+    snapToBins(x, binSize = 10) |>
+        expect_identical(GRanges("1:10-20"))
+
+    ## If exactly equal choose lowest bin to settle tie
+    x <- GRanges("1:9-11")
+    snapToBins(x, binSize = 10) |>
+        expect_identical(GRanges("1:0-10"))
+
+    x <- GRanges("1:6-14")
+    snapToBins(x, binSize = 10) |>
+        expect_identical(GRanges("1:0-10"))
+
+    ## If both ranges hit both midpoints then use 2 bins
+    x <- GRanges("1:5-15")
+    snapToBins(x, binSize = 10) |>
+        expect_identical(GRanges("1:0-20"))
+
+    x <- GRanges("1:3-15")
+    snapToBins(x, binSize = 10) |>
+        expect_identical(GRanges("1:0-20"))
+
+    x <- GRanges("1:5-17")
+    snapToBins(x, binSize = 10) |>
+        expect_identical(GRanges("1:0-20"))
+
+    x <- GRanges("1:15-35")
+    snapToBins(x, binSize = 10) |>
+        expect_identical(GRanges("1:10-40"))
+
+    ## Multiple values
+
+})
+
 test_that("GInteractions correctly snap to bins", {
 
     ## Sample GInteractions object
