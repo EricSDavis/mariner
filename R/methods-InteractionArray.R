a3c5eca149b64d6b2246ab069495ea7ea1d2b989
Eric Davis <ericscottdavis@outlook.com>
2023-04-13 16:56:40 -0400

    Update examples with test data from marinerData.

--- R/methods-InteractionArray.R
+++ R/methods-InteractionArray.R
@@ -82,10 +82,16 @@ setMethod("InteractionArray", c("missing", "missing"),
 #' ## Accessing Hi-C count submatrices ##
 #' ######################################
 #'
+#' ## Load marinerData
+#' if (!require("marinerData", quietly = TRUE))
+#'     install.packages("marinerData")
+#'
 #' ## Read .hic file paths
-#' hicFiles <-
-#'     system.file("extdata/test_hic", package="mariner") |>
-#'     list.files(pattern=".hic", full.names=TRUE)
+#' hicFiles <- c(
+#'     marinerData::LEUK_HEK_PJA27_inter_30.hic(),
+#'     marinerData::LEUK_HEK_PJA30_inter_30.hic()
+#' )
+#' names(hicFiles) <- c("FS", "WT")
 #'
 #' ## Create example interactions
 #' x <- read.table(text="
